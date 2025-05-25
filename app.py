f1ed9a73fba0efe9859be7a95a30a274064eeec0
Sebamon666 <sebastiansreyes@gmail.com>
2025-05-25 15:24:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ app.layout = html.Div([
         id='red-donaciones',
         elements=elements,
         layout={'name': 'cose'},
-        style={'width': '100%', 'height': '800px', 'margin': '0px'},
+        style={'position': 'fixed', 'top': 0, 'left': 0, 'right': 0, 'bottom': 0},
         stylesheet=[
             {
                 'selector': 'node',
